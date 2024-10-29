709f300a7a715a0d28616cd657884645ec543174
Kaspars L. <galtrhan@gmail.com>
2024-10-29 03:45:12 +0200

    Started working on install script

--- install.sh
+++ install.sh
@@ -1,40 +1,74 @@
 #!/bin/sh
 
 # List of packages to install
-packages=(
-  hyprland
-  hyprpaper
-  hypridle
-  hyprlock
-  waybar
-  kitty
-  fish
-  tmux
-  neovim
-  kakoune
-  stow
+pacman_packages = (
+	timeshift
+	yay
+	fish
+	hyprland
+	hyprcursor
+	hypridle
+	hyprlock
+	hyprpaper
+	waybar
+	wofi
+	dunst
+	kitty
+	nautilus
+	networkmanager
+	network-manager-applet
+	blueman
+	ttf-jetbrains-mono-nerd
+	tmux
+	neovim
+	kakoune
+	yazi
+	code
 )
+
+yay_packages = (
+	hyprpolkitagent-git
+)
+
+configs_to_remove = (
+	dunst
+	fish
+	hypr
+	kak
+	kitty
+	nvim
+	tmux
+	waybar
+	wlogout
+)
+
+# Check if sudo is available for root permissions, maybe not needed
+if ! command -v sudo >/dev/null 2>&1; then
+	echo "Error: sudo is not installed. Please install sudo first."
+	exit 1
+fi
 
 # Check if pacman is available
 if ! command -v pacman >/dev/null 2>&1; then
-  echo "Error: pacman is not installed or not in PATH."
-  exit 1
+	echo "Error: pacman is not installed or not in PATH."
+	exit 1
 fi
 
-# Check if sudo is available for root permissions
-if ! command -v sudo >/dev/null 2>&1; then
-  echo "Error: sudo is not installed. Please install sudo first."
-  exit 1
-fi
 
 # Update the package database
 echo "Updating package database..."
 sudo pacman -Syu --noconfirm
 
 # Install each package
-for package in "${packages[@]}"; do
-  echo "Installing $package..."
-  sudo pacman -S --noconfirm "$package"
+for package in "${pacman_packages[@]}"; do
+	echo "Installing $package..."
+	sudo pacman -S --noconfirm "$package"
+done
+
+# Remove default configs
+for config in "${configs_to_remove[@]}"; do
+	echo "Removing $config..."
+	sudo rm -rf "~/.config/$config"
 done
 
 # Activate dotfiles
